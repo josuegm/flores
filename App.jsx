ac6c12397aa9d44b4d8fd908e737b932b1a1585e
josuegm <mejia.josue07@gmail.com>
2023-09-22 06:31:15 -0600

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FloresAmarillas from './FloresAmarillas'; // Asegúrate de que la ruta del archivo sea correcta
+
+const App = () => {
+  return (
+    <div>
+      <FloresAmarillas />
+    </div>
+  );
+};
+
+export default App;
