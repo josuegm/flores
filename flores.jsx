344ce7d78f1c685d4a0483dd5043eb9f7e190c7e
josuegm <mejia.josue07@gmail.com>
2023-09-22 06:30:02 -0600

    Create flores.jsx

---
+++ flores.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FloresAmarillas = () => {
+  return (
+    <div>
+      <h1>Flores Amarillas para ti, mi amor</h1>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{ backgroundColor: 'yellow', borderRadius: '50%', width: '100px', height: '100px' }}></div>
+      </div>
+    </div>
+  );
+};
+
+export default FloresAmarillas;
